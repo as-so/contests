851a9edbc31c69e4fd125d825754b7cd3f9b0cfd
as-so <96018589+as-so@users.noreply.github.com>
2021-12-21 19:15:03 +0530

    Create stoll_to_string.cpp

---
+++ learnings/string & numbers/stoll_to_string.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    long long a = 1e18;
+    cout<<a<<endl;
+    string s = to_string(a);
+    cout<<s<<endl;
+    cout<<stoll("-1")<<endl;
+
+	return 0;
+}
+
